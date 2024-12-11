ff74cf06a58b129583142453451a8786f92f510b
Erick Gabriel <erickgabrielalves@hotmail.com>
2024-12-11 11:47:45 -0300

    feat: aula 2 completa

--- mobile/src/components/button/index.tsx
+++ mobile/src/components/button/index.tsx
@@ -3,12 +3,12 @@ import {
     TouchableOpacityProps,
     Text,
     TextProps,
-    ActivityIndicator
-} from "react-native";
-import { IconProps as TablerIconProprs } from "@tabler/icons-react-native";
+    ActivityIndicator,
+} from "react-native"
+import { IconProps as TablerIconProps } from "@tabler/icons-react-native"
 
-import { s } from "./styles";
-import { colors } from "@/styles/theme";
+import { s } from "./styles"
+import { colors } from "@/styles/theme"
 
 type ButtonProps = TouchableOpacityProps & {
     isLoading?: boolean
@@ -22,28 +22,28 @@ function Button({ children, style, isLoading = false, ...rest }: ButtonProps) {
             disabled={isLoading}
             {...rest}
         >
-            {
-                isLoading ? 
-                (<ActivityIndicator size="small" color={colors.gray[100]} />) : 
-                (children)
-            }
+            {isLoading ? (
+                <ActivityIndicator size="small" color={colors.gray[100]} />
+            ) : (
+                children
+            )}
         </TouchableOpacity>
-    );
+    )
 }
 
 function Title({ children }: TextProps) {
-    return <Text style={s.title}>{children}</Text>;
+    return <Text style={s.title}>{children}</Text>
 }
 
 type IconProps = {
-    icon: React.ComponentType<TablerIconProprs>
+    icon: React.ComponentType<TablerIconProps>
 }
 
 function Icon({ icon: Icon }: IconProps) {
     return <Icon size={24} color={colors.gray[100]} />
 }
 
-Button.Title = Title;
-Button.Icon = Icon;
+Button.Title = Title
+Button.Icon = Icon
 
-export { Button };
+export { Button }
